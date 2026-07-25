41edc47aefd093ec4bad0fd6a794d8a16d957876
Lrociy <1511171560@qq.com>
2026-07-26 03:04:03 +0800

    #test

--- main.py
+++ main.py
@@ -90,13 +90,15 @@ class UmaPlugins(Star):
 					skill_name = skill_list[int(idiom)]
 					chain = []
 					url = "https://wiki.biligame.com/umamusume/" + quote(f"简/{skill_name}")
-					logger.debug(f"查询技能url={url}")
 					async with aiohttp.ClientSession() as session:
 						async with session.get(url) as resp:
 							web = await resp.content.read()
 							context = BeautifulSoup(web)
+							logger.debug(f"查询技能网页内容={context}")
 							cards = context.find_all(style="position:relative;width:100px;margin:3px;")
+							logger.debug(f"查询技能网页卡片内容={cards}")
 							skill_data = context.find(class_="wikitable")
+							logger.debug(f"查询技能网页技能数据={skill_data}")
 							skill_info = skill_data and skill_data.find_all("td")
 							chain.append(Comp.Plain(text=f"类型={skill_info[2].string}代码={skill_info[5].string}描述={skill_info[6].string}类型={skill_info[7].string}数值={skill_info[8].string}时长={skill_info[9].string}"))
 							for card in cards:
